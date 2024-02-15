2815bc461cad3ffed6539b1007caf1fd9dccb6b5
Assylzhan Izbassar <a.izbassar@kbtu.kz>
2024-02-15 17:00:03 +0600

    thu15

---
+++ test/src/app/hero/hero.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+
+  sayHello() {
+    console.log("Hello!");
+  }
+}
