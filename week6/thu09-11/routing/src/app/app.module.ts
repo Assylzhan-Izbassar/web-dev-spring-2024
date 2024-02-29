08244a95fbb93737ec1840fe80001b1fb854b08a
Assylzhan Izbassar <a.izbassar@kbtu.kz>
2024-02-29 12:56:57 +0600

    week 7

--- week6/thu09-11/routing/src/app/app.module.ts
+++ week6/thu09-11/routing/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
@@ -14,7 +14,8 @@ import { ChildComponent } from './child/child.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [
     provideClientHydration()
--- week6/thu09-11/routing/src/app/parent/parent.component.ts
+++ week6/thu09-11/routing/src/app/parent/parent.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit {
   greeting: string = 'Hello!';
+  products?: Product[];
+
+  constructor(private productService: ProductService) {
+
+  }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  private loadData(): void {
+    this.productService.getProducts().subscribe({
+      next: (response: any) => {
+        this.products = response['products'];
+      }, 
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
 }
