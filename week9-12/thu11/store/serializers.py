ba44dc727fea5abb76195a9700d6715f71067213
Assylzhan Izbassar <a.izbassar@kbtu.kz>
2024-04-11 15:25:14 +0500

    week12

---
+++ week9-12/thu11/store/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2)
+
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
---
+++ week9-12/thu11/store/views.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductView(APIView):
+    def get(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'success'}, status=status.HTTP_201_CREATED)
+        return Response({'error', 'invalid data'}, status=status.HTTP_400_BAD_REQUEST)
