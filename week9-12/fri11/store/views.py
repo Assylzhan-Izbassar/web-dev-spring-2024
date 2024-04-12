341b485a9a37f8a0b8436cc3f76053716201caf3
Assylzhan Izbassar <a.izbassar@kbtu.kz>
2024-04-12 12:38:14 +0500

    fir11

---
+++ week9-12/fri11/store/views.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductView(APIView):
+    def get(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': 'Invalid data!'}, status=status.HTTP_400_BAD_REQUEST)
+    
