7a93ef761cbb853419b2dea277de4faf329d8bd5
Assylzhan Izbassar <a.izbassar@kbtu.kz>
2024-04-18 12:15:47 +0500

    thu11

--- week9-12/thu11/store/views.py
+++ week9-12/thu11/store/views.py
@@ -1,11 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS, BasePermission
 from .models import Product
 from .serializers import ProductSerializer
 
 
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+    
+
 class ProductView(APIView):
+    permission_classes = [IsAuthenticated|ReadOnly]
+
     def get(self, request):
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
